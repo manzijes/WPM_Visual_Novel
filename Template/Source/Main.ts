684b62c6266d31aaea7290f66a16275e4e8413be
manzijes <64071693+manzijes@users.noreply.github.com>
2022-10-20 18:56:04 +0200

    Update Main.ts
    testing main.ts structure

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -4,11 +4,58 @@ namespace Template {
 
   console.log("FudgeStory template starting");
 
+  export let transition = {
+      puzzle: {
+        duration: 1,
+        alpha: "",
+        edge: 1
+      }
+    };
+
+  export let sound = {
+    // themes
+    // SFX
+    drop: "../Audio/drop.mp3"
+  };
+
+  export let locations = {
+    beachDay: {
+      name: "Beach Day",
+      background: "",
+      foreground: ""
+    }
+  };
+
+  export let characters = {
+    narrator: {
+      name: ""
+    },
+    protagonist: {
+      name: ""
+    },
+    aisaka: {
+      name: "Aisaka",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "../Images/Characters/",
+        happy: "",
+        upset: ""
+      }
+    }
+  };
+
+  export let dataForSave = {
+    nameProtagonist: ""
+    };
+
   window.addEventListener("load", start);
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
       { scene: Scene, name: "Scene" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]");
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement);
 
     // start the sequence
     ƒS.Progress.go(scenes);
